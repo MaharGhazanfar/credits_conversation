1a6bd17ee139074892f3fd5f1b85daa0b432ae37
Olivi-tech <mshakilawan735@gmail.com>
2023-04-03 12:04:21 +0500

    added firebase for ios & android

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,12 @@ import 'package:credit_and_conversation/screens/screens.dart';
 import 'package:credit_and_conversation/utils/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
